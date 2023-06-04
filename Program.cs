aaf10b96c73ab2461cf8d501d872506e8c7f980f
S4-K1 <hardcastle.riley@gmail.com>
2023-06-05 00:15:06 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter first number:");
+
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter operator (+-*/)");
+
+string op = Console.ReadLine();
+
+Console.WriteLine("Enter second number:");
+
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double result = 0;
+
+switch(op) {
+	case "+":
+		result = num1 + num2;
+		break;
+	case "-":
+		result = num1 - num2;
+		break;
+	case "/":
+		result = num1 / num2;
+		break;
+	case "*":
+		result = num1 * num2;
+		break;
+}
+
+Console.WriteLine(result);
